ceed9b06d2ed448d16d742a06528de56199a30b1
Jose Deschamps <deschamps.jose001@gmail.com>
2017-06-10 02:57:53 -0400

    Now it begins!
    Time to work and focus!

---
+++ Source/ProjectKingBridge/Assets/Scripts/ClickMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickMotor : MonoBehaviour {
+
+
+
+	public Text goldDisplay;
+	public Text goldPerClickDisplay;
+	public float gold = 0;
+	public int goldPerClick = 1;
+
+
+	void Update(){
+
+		goldDisplay.text = "Gold: " + gold;
+		goldPerClickDisplay.text = "GPC: " + goldPerClick;
+
+	}
+
+
+	public void Clicked(){
+
+		gold += goldPerClick;
+
+
+	}
+}
---
+++ Source/ProjectKingBridge/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeManager : MonoBehaviour {
+
+
+	public ClickMotor clickmotor;
+	public Text itemInfo;
+	public string itemName;
+	public float cost;
+	public int count = 0;
+	public int clickPower;
+	private float baseCost;
+
+	void Start(){
+
+		baseCost = cost;
+
+	}
+
+	void Update(){
+
+		itemInfo.text = itemName + "\nCost: " + cost + "\nPower: +" + clickPower;
+
+	}
+
+
+	public void PurchasedUpgrade(){
+
+		if (clickmotor.gold >= cost) {
+			clickmotor.gold -= cost;
+			count += 1;
+			clickmotor.goldPerClick += clickPower;
+			cost = Mathf.Round (baseCost * Mathf.Pow (1.15f, count));
+
+
+
+//			cost = Mathf.Round (cost * 1.15f);
+//			baseCost = Mathf.Pow (cost, baseCost = cost);
+
+		}
+
+	}
+
+}
